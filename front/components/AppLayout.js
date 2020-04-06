d78c78734e3c1c8864230855fe09b7c32c533c92
Your Name <you@example.com>
2020-04-06 16:08:03 +0900

    Append : AppLayout Header

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,21 +1,79 @@
 import React from 'react';
-import { Menu, Input } from 'antd';
+import { Menu, Input, Form, Button } from 'antd';
 import styled from 'styled-components';
-
+const middleAlign = 'vertical-align: middle;';
 const SearchBar = styled(Input.Search)`
-  vertical-align: middle;
+  ${middleAlign}
+`;
+
+const Header = styled(Menu)`
+  padding: 0 100px;
+  &:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+`;
+
+const Signup = styled(Button)`
+  margin-left: 5px;
+  ${middleAlign}
+`;
+
+const Login = styled(Button)`
+  ${middleAlign}
+`;
+
+const LoginMenu = styled(Menu.Item)`
+  float: right;
+  padding-top: 5px;
+`;
+const Inputbox = styled(Input)`
+  ${middleAlign}
 `;
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <Menu mode="horizontal">
+      <Header mode="horizontal">
         <Menu.Item key="home">ReactSNS</Menu.Item>
         <Menu.Item key="profile">Profile</Menu.Item>
         <Menu.Item key="home">
           <SearchBar enterButton />
         </Menu.Item>
-      </Menu>
+        <LoginMenu key="login">
+          <Form layout="inline">
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Inputbox placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Inputbox type="password" placeholder="Password" />
+            </Form.Item>
+            <Form.Item>
+              <Login type="primary" htmlType="submit">
+                LOGIN
+              </Login>
+              <Signup htmlType="submit">SIGNUP</Signup>
+            </Form.Item>
+          </Form>
+        </LoginMenu>
+      </Header>
       {children}
     </div>
   );
