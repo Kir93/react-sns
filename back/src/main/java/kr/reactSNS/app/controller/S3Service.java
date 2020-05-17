16dd907c20b8a5e95c9ec0ff46320f445968ce23
Kir93 <cndfuf3484@naver.com>
2020-05-17 19:07:45 +0900

    tset

--- back/src/main/java/kr/reactSNS/app/controller/S3Service.java
+++ back/src/main/java/kr/reactSNS/app/controller/S3Service.java
@@ -11,6 +11,7 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import org.apache.commons.io.FilenameUtils;
@@ -49,8 +50,9 @@ public class S3Service {
                 String ext = FilenameUtils.getExtension(image.getOriginalFilename());
                 String basename = FilenameUtils.getBaseName(image.getOriginalFilename()) + new Date().getTime();
                 String newFile = basename + "." + ext;
-                System.out.println("여기일듯?");
-                s3Client.putObject(new PutObjectRequest(bucket, newFile, image.getInputStream(), null));
+                System.out.println("여기일듯?" + image.getInputStream());
+                s3Client.putObject(new PutObjectRequest(bucket, newFile, image.getInputStream(), null)
+                             .withCannedAcl(CannedAccessControlList.PublicRead));
                 System.out.println("맞지?");
                 // File dest = new File(baseDir + basename + "." + ext); // 개발환경
                 // image.transferTo(dest); // 개발환경
