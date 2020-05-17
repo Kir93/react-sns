d773476aa3d96cdb200eb32e69c346a87f2abef8
Kir93 <cndfuf3484@naver.com>
2020-05-17 23:52:38 +0900

    Finsh

--- back/src/main/java/kr/reactSNS/app/Application.java
+++ back/src/main/java/kr/reactSNS/app/Application.java
@@ -1,20 +1,17 @@
 package kr.reactSNS.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(value = { "classpath:${jdbc.config}" })
 public class Application {
 
-	@LocalServerPort
-    private static String port;
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("server is running on " + port);
+		System.out.println("server is running on");
 	}
 
 }
