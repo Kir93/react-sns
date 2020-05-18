0440bef048017ea28d07408828ae074401bf506d
Kir93 <cndfuf3484@naver.com>
2020-05-18 18:00:38 +0900

    Fix : S3 Path

--- back/src/main/java/kr/reactSNS/app/controller/PostController.java
+++ back/src/main/java/kr/reactSNS/app/controller/PostController.java
@@ -249,9 +249,9 @@ public class PostController {
                     ByteArrayOutputStream os = new ByteArrayOutputStream();
                     ImageIO.write(thumbImage, ext, os);
                     InputStream is = new ByteArrayInputStream(os.toByteArray());
-                    s3Client.putObject(new PutObjectRequest(bucket, "/original/"+newFile, byteArrayInputStream, objmeta)
+                    s3Client.putObject(new PutObjectRequest(bucket, "original/"+newFile, byteArrayInputStream, objmeta)
                                 .withCannedAcl(CannedAccessControlList.PublicRead));
-                    s3Client.putObject(new PutObjectRequest(bucket, "/thumbnail/"+newFile, is, null)
+                    s3Client.putObject(new PutObjectRequest(bucket, "thumbnail/"+newFile, is, null)
                                 .withCannedAcl(CannedAccessControlList.PublicRead));
                     // File dest = new File(baseDir + basename + "." + ext); // 개발환경
                     // image.transferTo(dest); // 개발환경
